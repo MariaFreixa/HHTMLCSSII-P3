38544b1b5bd040569d5c6c8ebeebd13ab20c3609
Maria Freixa Torres <mery.blood@gmail.com>
2021-05-23 13:29:35 +0200

    tailwind develop

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,38 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      header: ['Galatia SIL', 'Georgia', 'serif'],
+      body: ['Avenir Book', 'Franklin Gothic Medium', 'Helvetica', 'Arial', 'sans-serif'],
+    },
+    extend: {
+      container: {
+        center: true,
+        padding: "1rem"
+      },
+      colors: {
+        primary: '#B666D2',
+        fondo: '#f3f3f3',
+        hovertext: '#E0B0FF',
+      },
+      outline: {
+        purple: '2px solid #B666D2',
+      },
+      spacing: {
+        '75': '75%',
+      }
+    },
+    borderWidth: {
+      '1': '1px',
+      '2': '2px',
+      '5': '5px'
+    }
   },
   variants: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
-}
+  plugins: []
+};
